64e7a782bd756bb59964a40ad431fde52a2163e8
Elliot Marsden <elliot.marsden@gmail.com>
2017-07-01 23:22:58 +0200

    More

--- test.rs
+++ test.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::f64::consts;
+use std::fs::File;
+use std::io::prelude::*;
 
 struct RealPoint {
     x: f64,
@@ -67,6 +69,11 @@ impl IntPoint {
 
     fn add(&self, other: &IntPoint) -> IntPoint {
         IntPoint {i: other.i + self.i, j: other.j + self.j}
+    }
+
+    fn add_inplace(&mut self, other: &IntPoint) {
+        self.i += other.i;
+        self.j += other.j;
     }
 
     fn mul(&self, other: &IntPoint) -> IntPoint {
@@ -142,48 +149,25 @@ impl CBox {
 }
 
 fn get_force_mag(r_sq: i32) -> f64 {
-    // TODO: Stub.
-    1.0
+    50.0 - (r_sq as f64).sqrt()
 }
 
-fn force(du: IntPoint) -> RealPoint {
+fn force(du: &IntPoint) -> RealPoint {
     let force_mag = get_force_mag(du.mag_sq());
     du.to_unit().scale(force_mag)
 }
 
 impl CBox {
-    fn force_at(&self, o_from: usize, self_u: &IntPoint) -> RealPoint {
-        let mut f = RealPoint {x: 0.0, y: 0.0};
-        for i2 in 0..self.glyph.grid.len() {
-            for j2 in 0..self.glyph.grid[0].len() {
-                if self.glyph.grid[i2][j2] {
-                    let other_u = self.pos(o_from, i2, j2);
-                    let du = other_u.sub(self_u);
-                    f.add_inplace(&force(du));
-                }
-            }
-        }
-        f
-    }
-
     fn force_between(&self, o1: usize, o2: usize) -> RealPoint {
-        let mut f = RealPoint {x: 0.0, y: 0.0};
-        for i1 in 0..self.glyph.grid.len() {
-            for j1 in 0..self.glyph.grid[0].len() {
-                if self.glyph.grid[i1][j1] {
-                    let self_u = self.pos(o1, i1, j1);
-                    f.add_inplace(&self.force_at(o2, &self_u));
-                }
-            }
-        }
-        f
+        let du = self.origins[o2].sub(&self.origins[o1]);
+        force(&du)
     }
 
     fn pos(&self, o: usize, i: usize, j: usize) -> IntPoint {
         IntPoint {i: self.origins[o].i + i as i32, j: self.origins[o].j + j as i32}
     }
 
-    fn iterate(&self, dt: f64) {
+    fn iterate(&mut self, dt: f64) {
         for o1 in 0..self.origins.len() {
             let mut f12 = RealPoint {x: 0.0, y: 0.0};
             for o2 in 0..self.origins.len() {
@@ -192,59 +176,103 @@ impl CBox {
                 }
                 f12.add_inplace(&self.force_between(o1, o2));
             }
-            let dr = f12.scale(dt);
-            let du = dr.round();
-            self.origins[o1].add(&du);
+            self.origins[o1].add_inplace(&f12.scale(dt).round());
         }
     }
+
+    fn to_string(&mut self) -> String {
+        let mut s = String::new();
+        s += "glyph_dimens:\n";
+        s += &format!("gx,gy\n{},{}\n\n", self.glyph.grid.len(), self.glyph.grid[0].len());
+
+        s += "glyph_shape:\n";
+        for i in 0..self.glyph.grid.len() {
+            let strs: Vec<_> = self.glyph.grid[i].iter().map(|&e| format!("{}", e as u8)).collect();
+            s += &format!("{}\n", strs.join(","));
+        }
+
+        s += "\nnr_glyph_instances:\n";
+        s += &format!("{}\n", self.origins.len());
+
+        s += "\nglyph_instance_origins:\n";
+        s += "i,j\n";
+        for i in 0..self.origins.len() {
+            s += &format!("{},{}\n", self.origins[i].i, self.origins[i].j);
+        }        
+        s
+    }
+
 }
 
-fn add_cbox_to_grid(grid: &mut Vec<Vec<bool>>, cbox: &CBox) {
-    for i in 0..cbox.glyph.grid.len() {
-        for j in 0..cbox.glyph.grid[0].len() {
-            if cbox.glyph.grid[i][j] {
-                for origin_idx in 0..cbox.origins.len() {
-                    let pos = cbox.pos(origin_idx, i, j);
-                    grid[pos.i as usize][pos.j as usize] = true;
+struct Grid {
+    grid: Vec<Vec<bool>>,
+}
+
+impl Grid {
+    fn new(nx: usize, ny: usize) -> Grid {
+        Grid {grid: vec![vec![false; ny]; nx]}
+    }
+
+    fn add_cbox(&mut self, cbox: &CBox) {
+        for i in 0..cbox.glyph.grid.len() {
+            for j in 0..cbox.glyph.grid[0].len() {
+                if cbox.glyph.grid[i][j] {
+                    for origin_idx in 0..cbox.origins.len() {
+                        let pos = cbox.pos(origin_idx, i, j);
+                        self.grid[pos.i as usize][pos.j as usize] = true;
+                    }
                 }
             }
         }
     }
-}
 
-fn clear_grid(grid: &mut Vec<Vec<bool>>) {
-    for i in 0..grid.len() {
-        for j in 0..grid[0].len() {
-            grid[i][j] = false;
+    fn clear(&mut self) {
+        for i in 0..self.grid.len() {
+            for j in 0..self.grid[0].len() {
+                self.grid[i][j] = false;
+            }
         }
     }
+
+    fn to_string(&mut self) -> String {
+        let mut s = String::new();
+        for i in 0..self.grid.len() {
+            let strs: Vec<_> = self.grid[i].iter().map(|&e| format!("{}", e as u8)).collect();
+            s += &format!("{}\n", strs.join(","));
+        }
+        s
+    }
+
 }
 
-fn write_grid(grid: &Vec<Vec<bool>>) {
-    for i in 0..grid.len() {
-        let strs: Vec<_> = grid[i].iter().map(|&e| format!("{}", e as u8)).collect();
-        print!("{}\n", strs.join(","));
-    }
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let nx = 400;
-    let ny = 400;
+    let nx = 500;
+    let ny = 500;
 
     let circ = Circle {
-        radius: 0.05,
+        radius: 0.02,
     };
     let circ_glyph = circ.as_glyph(nx, ny);
     let mut cbox = circ_glyph.to_c_box();
+    cbox.add_origin(0.4, 0.4, nx, ny);
+    cbox.add_origin(0.6, 0.6, nx, ny);
     cbox.add_origin(0.2, 0.2, nx, ny);
-    cbox.add_origin(0.8, 0.8, nx, ny);
 
-    // let mut grid = vec![vec![false; ny]; nx];
-    for t in 0..20 {
-        cbox.iterate(0.01);
-        // clear_grid(&mut grid);
-        // add_cbox_to_grid(&mut grid, &cbox);
-        // write_grid(&grid);
+    let mut grid = Grid::new(nx, ny);
+    for t in 0..50 {
+        cbox.iterate(0.05);
+        println!("first: ({}, {})", cbox.origins[0].i, cbox.origins[0].j);
+
+        let mut f = File::create(format!("dat/out_{0:010}.csv", t)).expect("Could not create file");
+        // grid.clear();
+        // grid.add_cbox(&cbox);
+        // f.write_all(grid.to_string().as_bytes()).expect("Failed to write message");
+        let mut s = String::new();
+        s += "grid_dimens:\n";
+        s += &format!("nx,ny\n{},{}\n\n", nx, ny);
+        s += &cbox.to_string();
+        f.write_all(s.as_bytes()).expect("Failed to write message");
     }
 }
--- view.py
+++ view.py
@@ -1,11 +1,64 @@
-# import matplotlib.pyplot as plt
-from bokeh.plotting import figure, show, output_file
+from glob import glob
+
+import matplotlib.pyplot as plt
+# from bokeh.plotting import figure, show, output_file
 import numpy as np
 
-d = np.loadtxt('out.csv', delimiter=',', dtype=np.uint8)
-# plt.imshow(d)
+fig, ax = plt.subplots()
 
-p = figure(x_range=(0, 1), y_range=(0, 1))
-p.image(image=[d], x=0, y=0, dw=1, dh=1, palette="Spectral11")
+def parse(f):
+    line = f.readline()
+    assert line == 'grid_dimens:\n', repr(line)
+    line = f.readline()
+    assert line == 'nx,ny\n', line
+    grid_dimens = list(map(int, f.readline().split(',')))
 
-show(p)
+    f.readline()
+
+    line = f.readline()
+    assert line == 'glyph_dimens:\n', line
+    line = f.readline()
+    assert line == 'gx,gy\n', line
+    glyph_dimens = list(map(int, f.readline().split(',')))
+
+    f.readline()
+
+    line = f.readline()
+    assert line == 'glyph_shape:\n', line
+    glyph_shape_lst = [list(map(int, f.readline().split(','))) for _ in range(glyph_dimens[1])]
+    glyph = np.array(glyph_shape_lst, dtype=np.bool)
+
+    f.readline()
+
+    line = f.readline()
+    assert line == 'nr_glyph_instances:\n', line
+    nr_glyphs = int(f.readline())
+
+    f.readline()
+
+    line = f.readline()
+    assert line == 'glyph_instance_origins:\n', line
+    line = f.readline()
+    assert line == 'i,j\n', line
+    glyph_origins_lst = [list(map(int, f.readline().split(','))) for _ in range(nr_glyphs)]
+    glyph_origins = np.array(glyph_origins_lst, dtype=np.int)
+    return grid_dimens, glyph, glyph_origins
+
+file_names = sorted(glob('dat/*.csv'))
+for file_name in file_names:
+    f = open(file_name)
+    grid_dimens, glyph, glyph_origins = parse(f)
+    
+    grid = np.zeros(grid_dimens, dtype=np.bool)
+    for glyph_origin in glyph_origins:
+        glyph_termin = glyph_origin + glyph.shape
+        grid[glyph_origin[0]:glyph_termin[0], glyph_origin[1]:glyph_termin[1]] += glyph
+
+    ax.cla()
+    ax.imshow(grid)
+    fig.savefig(file_name.replace('csv', 'png'))
+
+# p = figure(x_range=(0, 1), y_range=(0, 1))
+# p.image(image=[d], x=0, y=0, dw=1, dh=1, palette="Spectral11")
+
+# show(p)
