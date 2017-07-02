98e417239f84a1ab785ae6c61375684b6391d3b6
Elliot Marsden <elliot.marsden@gmail.com>
2017-07-02 19:27:18 +0200

    More

--- test.rs
+++ test.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::f64::consts;
 use std::fs::File;
 use std::io::prelude::*;
+use std::collections::HashMap;
 
 struct RealPoint {
     x: f64,
@@ -112,39 +112,22 @@ impl Circle {
     }
 }
 
-
 struct Glyph {
     grid: Vec<Vec<bool>>,
 }
 
-impl Glyph {
-    fn to_h_box(self, base_line: f64) -> HBox {
-        HBox {
-            origins: vec![],
-            glyph: self,
-            base_line: base_line,
-        }
-    }
+struct CBox {
+    glyph_name: String,
+    origin: RealPoint,
 }
 
 struct HBox {
-    origins: Vec<RealPoint>,
-    glyph: Glyph,
+    c_boxes: Vec<CBox>,
     base_line: f64,
 }
 
-impl HBox {
-    fn add_origin(&mut self, x: f64, y: f64) {
-        let origin = RealPoint {
-            x: x,
-            y: y,
-        };
-        self.origins.push(origin);
-    }
-}
-
 fn get_spring_force_mag(r: f64, k: f64) -> f64 {
-    k * r
+    -k * r
 }
 
 const R_GLYPH: f64 = 0.1;
@@ -153,6 +136,14 @@ const K_LINE: f64 = 0.05;
 const R_LINE: f64 = 0.4;
 
 impl HBox {
+    fn add_c_box(&mut self, glyph_name: String, x: f64, y: f64) {
+        let origin = RealPoint {
+            x: x,
+            y: y,
+        };
+        self.c_boxes.push(CBox {glyph_name: glyph_name, origin: origin});
+    }
+
     fn force_between(&self, o1: i32, o2: i32) -> RealPoint {
         if o1 == o2 {
             RealPoint {
@@ -161,12 +152,12 @@ impl HBox {
             }
         } else if o2 == o1 + 1 {
             RealPoint {
-                x: -get_spring_force_mag(self.origins[o1 as usize].x - (self.origins[o2 as usize].x + R_GLYPH), K_GLYPH),
+                x: get_spring_force_mag(self.c_boxes[o1 as usize].origin.x - (self.c_boxes[o2 as usize].origin.x + R_GLYPH), K_GLYPH),
                 y: 0.0,
             }
         } else if o2 == o1 - 1 {
             RealPoint {
-                x: -get_spring_force_mag(self.origins[o1 as usize].x - (self.origins[o2 as usize].x - R_GLYPH), K_GLYPH),
+                x: get_spring_force_mag(self.c_boxes[o1 as usize].origin.x - (self.c_boxes[o2 as usize].origin.x - R_GLYPH), K_GLYPH),
                 y: 0.0,
             }
         } else {
@@ -180,49 +171,39 @@ impl HBox {
     fn force_line(&self, o1: usize) -> RealPoint {
         RealPoint {
             x: 0.0,
-            y: -get_spring_force_mag(self.origins[o1].y - self.base_line, K_LINE),
-            // y: 0.0,
+            y: get_spring_force_mag(self.c_boxes[o1].origin.y - self.base_line, K_LINE),
         }
     }
 
     fn iterate(&mut self, dt: f64) {
-        for o1 in 0..self.origins.len() {
+        for o1 in 0..self.c_boxes.len() {
             let mut f1 = RealPoint {x: 0.0, y: 0.0};
-            for o2 in 0..self.origins.len() {
+            for o2 in 0..self.c_boxes.len() {
                 f1.add_inplace(&self.force_between(o1 as i32, o2 as i32));
             }
             f1.add_inplace(&self.force_line(o1));
-            self.origins[o1].add_inplace(&f1.scale(dt));
+            self.c_boxes[o1].origin.add_inplace(&f1.scale(dt));
         }
     }
 
     fn to_string(&self) -> String {
         let mut s = String::new();
-        s += "glyph_dimens:\n";
-        s += &format!("gx,gy\n{},{}\n\n", self.glyph.grid.len(), self.glyph.grid[0].len());
 
-        s += "glyph_shape:\n";
-        for i in 0..self.glyph.grid.len() {
-            let strs: Vec<_> = self.glyph.grid[i].iter().map(|&e| format!("{}", e as u8)).collect();
-            s += &format!("{}\n", strs.join(","));
+        s += "\nnr_c_boxes:\n";
+        s += &format!("{}\n\n", self.c_boxes.len());
+
+        s += "glyph_name,x,y\n";
+        for c_box in &self.c_boxes {
+            s += &format!("{},{},{}\n", c_box.glyph_name, c_box.origin.x, c_box.origin.y);
         }
-
-        s += "\nnr_glyph_instances:\n";
-        s += &format!("{}\n", self.origins.len());
-
-        s += "\nglyph_instance_origins:\n";
-        s += "x,y\n";
-        for i in 0..self.origins.len() {
-            s += &format!("{},{}\n", self.origins[i].x, self.origins[i].y);
-        }        
         s
     }
-
 }
 
 struct Page {
     shape: IntPoint,
     h_boxes: Vec<HBox>,
+    glyphs: HashMap<String,Glyph>,
 }
 
 impl Page {
@@ -230,13 +211,32 @@ impl Page {
         Page {
             shape: IntPoint {i: nx, j: ny},
             h_boxes: vec![],
+            glyphs: HashMap::new(),
         }
     }
 
     fn to_string(&self) -> String {
         let mut s = String::new();
         s += "grid_dimens:\n";
-        s += &format!("nx,ny\n{},{}\n\n", self.shape.i, self.shape.j);
+        s += &format!("nx,ny\n{},{}\n", self.shape.i, self.shape.j);
+
+        s += "\nnr_glyphs:\n";
+        s += &format!("{}\n\n", self.glyphs.len());
+
+        for (glyph_name, glyph) in &self.glyphs {
+            s += "glyph_name:\n";
+            s += &format!("{}\n", glyph_name);
+
+            s += "glyph_dimens:\n";
+            s += &format!("gx,gy\n{},{}\n", glyph.grid.len(), glyph.grid[0].len());
+
+            s += "glyph_shape:\n";
+            for grid_row in &glyph.grid {
+                let strs: Vec<_> = grid_row.iter().map(|&e| format!("{}", e as u8)).collect();
+                s += &format!("{}\n", strs.join(","));
+            }
+        }
+
         s += &self.h_boxes[0].to_string();
         s
     }
@@ -255,18 +255,26 @@ fn main() {
         radius: 0.02,
     };
     let circ_glyph = circ.as_glyph(nx, ny);
-    let mut h_box = circ_glyph.to_h_box(R_LINE);
-    h_box.add_origin(0.11, 0.23);
-    h_box.add_origin(0.67, 0.6);
-    h_box.add_origin(0.31, 0.4);
-    h_box.add_origin(0.77, 0.8);
+
+    let circ_glyph_name = String::from("circle");
+    page.glyphs.insert(circ_glyph_name.clone(), circ_glyph);
+
+    let mut h_box = HBox {
+        c_boxes: vec![],
+        base_line: R_LINE,
+    };
+
+    h_box.add_c_box(circ_glyph_name.clone(), 0.11, 0.23);
+    h_box.add_c_box(circ_glyph_name.clone(), 0.67, 0.6);
+    h_box.add_c_box(circ_glyph_name.clone(), 0.31, 0.4);
+    h_box.add_c_box(circ_glyph_name.clone(), 0.77, 0.8);
 
     page.h_boxes.push(h_box);
 
     for t in 0..10000 {
         page.h_boxes[0].iterate(0.01);
         if t % 50 == 0 {
-            println!("hihi");
+            // println!("hihi");
             let mut f = File::create(format!("dat/out_{0:010}.csv", t)).expect("Could not create file");
             f.write_all(page.to_string().as_bytes()).expect("Failed to write message");                        
         }
--- view.py
+++ view.py
@@ -4,8 +4,6 @@ import matplotlib.pyplot as plt
 # from bokeh.plotting import figure, show, output_file
 import numpy as np
 
-fig, ax = plt.subplots()
-
 def parse(f):
     line = f.readline()
     assert line == 'grid_dimens:\n', repr(line)
@@ -16,52 +14,66 @@ def parse(f):
     f.readline()
 
     line = f.readline()
-    assert line == 'glyph_dimens:\n', line
-    line = f.readline()
-    assert line == 'gx,gy\n', line
-    glyph_dimens = list(map(int, f.readline().split(',')))
-
-    f.readline()
-
-    line = f.readline()
-    assert line == 'glyph_shape:\n', line
-    glyph_shape_lst = [list(map(int, f.readline().split(','))) for _ in range(glyph_dimens[1])]
-    glyph = np.array(glyph_shape_lst, dtype=np.bool)
-
-    f.readline()
-
-    line = f.readline()
-    assert line == 'nr_glyph_instances:\n', line
+    assert line == 'nr_glyphs:\n', line
     nr_glyphs = int(f.readline())
 
     f.readline()
 
+    glyphs = {}
+    for _ in range(nr_glyphs):
+        line = f.readline()
+        assert line == 'glyph_name:\n', line
+        glyph_name = f.readline().strip()
+        line = f.readline()
+        assert line == 'glyph_dimens:\n', line
+        line = f.readline()
+        assert line == 'gx,gy\n', line
+        glyph_dimens = list(map(int, f.readline().split(',')))
+        line = f.readline()
+        assert line == 'glyph_shape:\n', line
+        glyph_shape_lst = [list(map(int, f.readline().split(','))) for _ in range(glyph_dimens[1])]
+        glyph = np.array(glyph_shape_lst, dtype=np.bool)
+        glyphs[glyph_name] = glyph
+        f.readline()
+
     line = f.readline()
-    assert line == 'glyph_instance_origins:\n', line
+    assert line == 'nr_c_boxes:\n', line
+    nr_c_boxes = int(f.readline())
+
+    f.readline()
+
     line = f.readline()
-    assert line == 'x,y\n', line
-    glyph_origins_lst = [
-        list(map(float, f.readline().split(',')))
-        for _ in range(nr_glyphs)
-    ]
-    glyph_origins = np.array(glyph_origins_lst, dtype=np.float)
-    return grid_dimens, glyph, glyph_origins
+    assert line == 'glyph_name,x,y\n', line
+
+    c_boxes = []
+    for _ in range(nr_c_boxes):
+        parts = f.readline().split(',')
+        glyph_name = parts[0].strip()
+        origin = np.array(list(map(float, parts[1:])))
+        c_box = (glyph_name, origin)
+        c_boxes.append(c_box)
+    return grid_dimens, glyphs, c_boxes
 
 file_names = sorted(glob('dat/*.csv'))
+
+grid_dimens, _, _ = parse(open(file_names[0]))
+fig, ax = plt.subplots()
+im = ax.imshow(np.zeros(grid_dimens, dtype=np.bool), vmin=0, vmax=1)
+
 for file_name in file_names:
     print(file_name)
     f = open(file_name)
-    grid_dimens, glyph, glyph_origins = parse(f)
-
-    glyph_origins_i = (glyph_origins * grid_dimens).round().astype(np.int)
+    grid_dimens, glyphs, c_boxes = parse(f)
 
     grid = np.zeros(grid_dimens, dtype=np.bool)
-    for glyph_origin_i in glyph_origins_i:
-        glyph_termin_i = glyph_origin_i + glyph.shape
-        grid[glyph_origin_i[0]:glyph_termin_i[0], glyph_origin_i[1]:glyph_termin_i[1]] += glyph
 
-    ax.cla()
-    ax.imshow(grid.T)
+    for glyph_name, origin in c_boxes:
+        glyph = glyphs[glyph_name]
+        origin_i = (origin * grid_dimens).round().astype(np.int)
+        termin_i = origin_i + glyph.shape
+        grid[origin_i[0]:termin_i[0], origin_i[1]:termin_i[1]] += glyph
+
+    im.set_data(grid.T)
     fig.savefig(file_name.replace('csv', 'png'))
 
 # p = figure(x_range=(0, 1), y_range=(0, 1))
